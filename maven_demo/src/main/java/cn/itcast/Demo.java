3a49d0b8f0f9a3295c587c350d14e04f9939c9be
123 <123@163.com>
2018-11-24 21:42:12 +0800

    修改

--- maven_demo/src/main/java/cn/itcast/Demo.java
+++ maven_demo/src/main/java/cn/itcast/Demo.java
@@ -3,5 +3,6 @@ package cn.itcast;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello,git");
+        System.out.println("aaaaaaaaaaaaaa");
     }
 }
